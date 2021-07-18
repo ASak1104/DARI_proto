c8328c7ebea1083e6b8deb0aaa68b2a5123f7363
ASak1104 <khw56184@gmail.com>
2021-07-19 01:44:26 +0900

    Commit left out profile.js file

---
+++ Server/routes/profile.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const { isSignedIn, isNotSignedIn } = require('./middlewares');
+const User = require('../schemas/user');
+const Interest = require('../schemas/interest');
+const UserToInterest = require('../schemas/userToInterest');
+
+const router = express.Router();
+
+
+/* POST user/:id/profile/interest page */
+// need to add MW
+router.post('/:id/profile/interest', async (req, res, next) => {
+    const { interests } = req.body;
+    try {
+        const user = await User.findOne( { userId: req.params.id }, '_id').lean();
+        await Promise.all(interests.map(async (item) => {
+            const interest = await Interest.findOne({ name: item }, '_id').lean();
+            await UserToInterest.create({
+                user: user._id,
+                interest: interest._id,
+            })
+        }));
+        res.status(201).json({ created: true });
+    } catch (err) {
+        console.log(err);
+        return next(err);
+    }
+});
+
+
+/* PUT user/:id/profile/interest page */
+router.put('/:id/profile/interest', async (req, res, next) => {
+    const { interests } = req.body;
+    try {
+        const _id = await User.findOne( { userId: req.params.id }, '_id').lean()
+            .then((user) => user._id);
+        const newInterests = await Promise.all(interests.map(async (name) => {
+            return await Interest.findOne({ name }, '_id').lean()
+                .then((obj) => obj._id);
+        }));
+
+        const deleteOldUserInterest = async () => {
+            await UserToInterest.deleteMany({ user: _id ,interest: { $nin: newInterests } });
+        };
+
+        const createNewUserInterest = async () => {
+            await Promise.all(newInterests.map(async (interest) => {
+                await UserToInterest.findOneAndUpdate({ user: _id, interest }, {}, { upsert: true });
+            }));
+        };
+
+        await Promise.all([deleteOldUserInterest(), createNewUserInterest()]);
+
+        res.status(202).json({ updated: true });
+    } catch (err) {
+        console.log(err);
+        return next(err);
+    }
+});
+
+
+module.exports = router;

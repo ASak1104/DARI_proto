441c6ad9ce6294bd2cdcf1c12fb79b0fba792e98
daisophila <daisophila@gmail.com>
2021-07-19 14:46:52 +0900

    동네인증 업데이트 추가2

--- DARI_APP/app/src/main/java/com/example/app_dari/ProfileUpRq.java
+++ DARI_APP/app/src/main/java/com/example/app_dari/ProfileUpRq.java
@@ -1,5 +1,11 @@
 package com.example.app_dari;
 
-public class ProfileUpRq {
+import com.google.gson.annotations.SerializedName;
 
+public class ProfileUpRq {
+    @SerializedName("status")
+    public int resultCode;
+
+    @SerializedName("name")
+    public String name;
 }
--- DARI_APP/app/src/main/java/com/example/app_dari/RetrofitService.java
+++ DARI_APP/app/src/main/java/com/example/app_dari/RetrofitService.java
@@ -1,14 +1,20 @@
 package com.example.app_dari;
 
 
+import java.util.HashMap;
+
 import retrofit2.Call;
+import retrofit2.http.FieldMap;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface RetrofitService {
     @GET("map/{post}")
     Call<MapData> getPosts(@Path("post") String post);
 
+    @POST("/posts")
+    Call<ProfileUpRq> postData(@FieldMap HashMap<String, Object> param);
 
 
 }

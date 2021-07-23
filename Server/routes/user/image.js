e4dc9b651dd1928c02ae6cb3d0e8185e2e701012
ASak1104 <khw56184@gmail.com>
2021-07-24 00:58:51 +0900

    Resolve image url problem

--- Server/routes/user/image.js
+++ Server/routes/user/image.js
@@ -4,24 +4,28 @@ const path = require('path');
 const fs = require('fs');
 
 const { getDate } = require('../middlewares');
+const imageRepo = 'uploads'
 
 const router = express.Router();
 
 try {
-    fs.readdirSync('uploads');
+    fs.readdirSync(imageRepo);
 } catch (error) {
     console.error('Make uploads repository');
-    fs.mkdirSync('uploads');
+    fs.mkdirSync(imageRepo);
 }
 
 const upload = multer({
     storage: multer.diskStorage({
         destination(req, file, cb) {
-            cb(null, 'uploads/');
+            cb(null, imageRepo);
         },
         filename(req, file, cb) {
             const ext = path.extname(file.originalname);
-            cb(null, req.params.id + ext);
+            if(!['.png', '.jpg', '.jpeg', '.gif'].includes(ext)) {
+                return cb(new Error('Only images are allowed'));
+            }
+            cb(null, req.params.id + '.jpg');
         },
     }),
     limits: { fileSize: 5 * 1024 * 1024 },
@@ -29,24 +33,23 @@ const upload = multer({
 
 
 /* GET user/:image page */
-// need to change url
-router.get('/:image', (req, res) => {
-    const ext = path.extname(path.join(__dirname, `../../uploads/${req.params.image}`));
-    res.sendFile(path.join(__dirname, `../../uploads/${req.params.image}`));
+router.get('/:id/image', (req, res) => {
+    const ext = path.extname(path.join(__dirname, `../../${imageRepo}/${req.params.image}`));
+    res.sendFile(path.join(__dirname, `../../${imageRepo}/${req.params.id}.jpg`));
 });
 
 
 /* POST user/image page */
-router.post('/image', upload.single('image'), (req, res) => {
+router.post('/:id/image', upload.single('image'), (req, res) => {
     console.log(req.file);
-    res.json({ url: `user/${req.file.filename}` });
+    res.json({ url: `user/${req.params.id}/image` });
 });
 
 
 /* PUT user/image page */
-router.put('/image', upload.single('image'), (req, res) => {
+router.put('/:id/image', upload.single('image'), (req, res) => {
     console.log(req.file);
-    res.json({ url: `user/${req.file.filename}` });
+    res.json({ url: `user/${req.params.id}/image` });
 });
 
 

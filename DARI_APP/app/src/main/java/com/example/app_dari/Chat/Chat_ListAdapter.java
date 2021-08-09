278885d104f163126db9c885f3e8a8d6bad2116e
daisophila <daisophila@gmail.com>
2021-08-09 22:58:50 +0900

    3

--- DARI_APP/app/src/main/java/com/example/app_dari/Chat/Chat_ListAdapter.java
+++ DARI_APP/app/src/main/java/com/example/app_dari/Chat/Chat_ListAdapter.java
@@ -1,7 +1,7 @@
 package com.example.app_dari.Chat;
 
+import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,26 +11,22 @@ import android.widget.TextView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.MultiTransformation;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.load.model.LazyHeaders;
-import com.bumptech.glide.load.resource.bitmap.CenterCrop;
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.example.app_dari.Login.LoginActivity;
 import com.example.app_dari.R;
-import com.example.app_dari.UserStatic;
 
 import java.util.ArrayList;
-
 public class Chat_ListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private ArrayList<Chat_List_Data> mDataSet;
+    public Activity context;
 
 
 
-
-    public Chat_ListAdapter(ArrayList<Chat_List_Data> myDataSet){
+    public Chat_ListAdapter(ArrayList<Chat_List_Data> myDataSet, Activity context){
         mDataSet = myDataSet;
+        this.context = context;
 
     }
 
@@ -43,26 +39,26 @@ public class Chat_ListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
         LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
 
-            v = inflater.inflate(R.layout.chat_list_item,parent,false);
-            return new ViewHolder(v);
+        v = inflater.inflate(R.layout.chat_list_item,parent,false);
+        return new ViewHolder(v);
 
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder,int position){
-            ((ViewHolder) holder).name_text.setText(mDataSet.get(position).getUserNameTitle());
+        ((ViewHolder) holder).name_text.setText(mDataSet.get(position).getUserNameTitle());
         ((ViewHolder) holder).msg_text.setText(mDataSet.get(position).getLastMessage());
         ((ViewHolder) holder).send_time.setText(mDataSet.get(position).getUpdatedAt().substring(11,16));
 
-        /*GlideUrl glideUrl = new GlideUrl("http://dari-app.kro.kr/user/image/"+mDataSet.get(position).getUserNameTitle() , new LazyHeaders.Builder()
-                .addHeader("authorization", UserStatic.token)
+        GlideUrl glideUrl = new GlideUrl("http://dari-app.kro.kr/user/image/"+mDataSet.get(position).getUserNameTitle() , new LazyHeaders.Builder()
+                .addHeader("authorization", LoginActivity.token)
                 .build());
-        Glide.with(((ViewHolder) holder).chat_list_img.getContext())
+        Glide.with(context)
                 .asBitmap()
                 .diskCacheStrategy(DiskCacheStrategy.NONE)
                 .load(glideUrl)
                 .centerCrop()
-                .into(((ViewHolder) holder).chat_list_img);*/
+                .into(((ViewHolder) holder).chat_list_img);
 
     }
 
@@ -108,3 +104,4 @@ public class Chat_ListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
 
 
 }
+
